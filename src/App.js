149633c4d78173c70e5e2a97ed9f8d77b30a90e1
anwarhossain1 <irishkhan33@gmail.com>
2020-12-02 19:23:58 +0600

    Save before ejecting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{Component} from 'react';
+class App extends Component {
+  change=(e)=>{
+    console.log("clicked");
+}
+  render(){
+  
+    return (
+      <div>
+        <h1 className="setcomp">My App</h1>
+        <span>
+          <button onClick={this.change}>Press</button>
+        </span>
+      </div>
+      
+    );
+
+  }
+ 
 }
 
 export default App;
